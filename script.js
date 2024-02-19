474c662b18da5ed131d8210ad06d6c5e2fa0a7c1
debdebich <156684402+debdebich@users.noreply.github.com>
2024-02-19 16:49:14 +0200

    added active on scroll

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// window.onscroll = function(){
-//     const h = document.documentElement.clientHeight;
-//     console.log(h);
-// };
-
-
+let classLink = '.main-link'
 const btns = document.querySelectorAll('.menu__btn a');
+
+
+window.onscroll = function(){
+    const h = document.documentElement.clientHeight;
+
+    if (window.scrollY >= h*4) classLink = '.feedback-link';
+    else if (window.scrollY >= h*3) classLink = '.projects-link';
+    else if (window.scrollY >= h*2) classLink = '.skills-link';
+    else if (window.scrollY >= h) classLink = '.about-link';
+    else classLink = '.main-link';
+
+    let activeBtn = document.querySelector('.active');
+    let newActiveBtn = document.querySelector(classLink);
+    if (!newActiveBtn.classList.contains('active')) {
+        newActiveBtn.classList.add('active');
+        activeBtn.classList.remove('active');
+    };
+    console.log(classLink);
+    console.log(newActiveBtn);
+};
+
 
 for (let btn of btns){
     btn.addEventListener('click', function(event){
